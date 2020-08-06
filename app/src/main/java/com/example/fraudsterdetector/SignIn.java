43c6d164885065974f1b4b2d3147bc890346f2e0
Raghav Ambashta <raghav.ambashta22@gmail.com>
2020-08-06 15:08:46 +0530

    activity_sign_in_updated

--- app/src/main/java/com/example/fraudsterdetector/SignIn.java
+++ app/src/main/java/com/example/fraudsterdetector/SignIn.java
@@ -3,12 +3,17 @@ package com.example.fraudsterdetector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
+
+    EditText signInPhoneEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        signInPhoneEditText = (EditText) findViewById(R.id.signInPhoneEditText);
     }
 }
